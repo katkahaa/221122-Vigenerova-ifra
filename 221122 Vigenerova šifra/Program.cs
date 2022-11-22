e2e623bd7e8dd945ef9dabe65dd92ac60d468c2f
haasova.ka.2019 <haasova.ka.2019@UC313PC05>
2022-11-22 15:21:49 +0100

    Přidejte soubory projektu.

---
+++ 221122 Vigenerova šifra/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221122_Vigenerova_šifra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Encrypt("salamander", "quark");
+            Console.WriteLine(message);
+            Console.WriteLine(Decrypt(message, "quark"));
+            Console.ReadLine();
+        }
+
+        static string Encrypt (string message, string key)
+        {
+            string encryptedMessage = "";
+            int iKey = 0;
+            int keyIndex = 0;
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i] >= 97 || message[i] <= 122)
+                {
+                    keyIndex = i;
+                    if (keyIndex >= key.Length)
+                        keyIndex -= key.Length;
+                    iKey = key[keyIndex];
+
+                    int c = message[i] + (iKey - 'a');
+                    if (c > 122)
+                        c -= 26;
+                    encryptedMessage += (char)c;
+                }
+            }
+
+            return encryptedMessage;
+        }
+
+        static string Decrypt(string message, string key)
+        {
+            string decryptedMessage = "";
+            int iKey = 0;
+            int keyIndex = 0;
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i] >= 97 || message[i] <= 122)
+                {
+                    keyIndex = i;
+                    if (keyIndex >= key.Length)
+                        keyIndex -= key.Length;
+                    iKey = key[keyIndex];
+
+                    int c = message[i] - (iKey - 'a');
+                    if (c < 97)
+                        c += 26;
+                    decryptedMessage += (char)c;
+                }
+            }
+
+            return decryptedMessage;
+        }
+    }
+}
